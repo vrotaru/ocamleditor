6fb68eb24183d65bb08ce9e795928e73bd6d3427
Vasile Rotaru <vrotaru.md@gmail.com>
2020-09-15 00:26:18 +0300

    Small cmt_view cleanup

--- src/autocomp.ml
+++ src/autocomp.ml
@@ -140,7 +140,7 @@ let compile_buffer ~project ~editor ~page ?(join=false) () =
             Gmisclib.Idle.add ~prio:100 begin fun () ->
               match page#outline with
                 | None ->
-                  let ol = new Cmt_view.widget ~editor ~page () in
+                  let ol = new Cmt_view.widget ~page () in
                   ol#load ();
                   Gaux.may page#outline ~f:(fun x -> x#destroy());
                   page#set_outline (Some ol);
--- src/cmt_view.ml
+++ src/cmt_view.ml
@@ -156,7 +156,7 @@ let empty () =
 let dummy_re = Str.regexp ""
 
 (** widget *)
-class widget ~editor:_ ~page ?packing () =
+class widget ~page ?packing () =
   let pref                   = Preferences.preferences#get in
   let show_types             = pref.Preferences.pref_outline_show_types in
   let vbox                   = GPack.vbox ?packing () in
@@ -1091,10 +1091,10 @@ end
 
 let create = new widget
 
-let window ~editor ~page () =
+let window ~page () =
   let window = GWindow.window ~position:`CENTER ~show:false () in
   let vbox = GPack.vbox ~packing:window#add () in
-  let widget = create ~editor ~page ~packing:vbox#add () in
+  let widget = create ~page ~packing:vbox#add () in
   ignore (window#event#connect#key_press ~callback:begin fun ev ->
     if GdkEvent.Key.keyval ev = GdkKeysyms._Escape then (window#destroy (); true)
     else false
